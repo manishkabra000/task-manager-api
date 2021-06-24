f6bcd18d14c11a406182705deed560b9e42096fb
Manish Kabra <manishkabra000@gmail.com>
2021-06-25 00:05:03 +0530

    accepting auth token

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const User = require("../models/user");
 const router = new express.Router();
+const auth = require("../middleware/auth");
 
 router.post("/users", async (req, res) => {
   const user = new User(req.body);
@@ -26,13 +27,8 @@ router.post("/users/login", async (req, res) => {
   }
 });
 
-router.get("/users", async (req, res) => {
-  try {
-    const user = await User.find({});
-    res.send(user);
-  } catch (err) {
-    res.status(500).send(err);
-  }
+router.get("/users/me", auth, async (req, res) => {
+  res.send(req.user);
 });
 
 router.get("/users/:id", async (req, res) => {
