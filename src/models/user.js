2917f2fbcb23c4b3b3c9af9733ff28efb7488942
Manish Kabra <manishkabra000@gmail.com>
2021-06-24 22:26:51 +0530

    generate auth token

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -39,8 +41,25 @@ const userSchema = new mongoose.Schema({
       }
     },
   },
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
 });
+// Methods are accessible on instance => Instance Methods
+userSchema.methods.generateAuthToken = async function () {
+  const user = this;
+  const token = jwt.sign({ _id: user._id.toString() }, "thisismynewtoken");
+  user.tokens = user.tokens.concat({ token });
+  await user.save();
+  return token;
+};
 
+// Statics are accessible on Models => Model Methods
 userSchema.statics.findByCredentials = async (email, password) => {
   const user = await User.findOne({ email });
   if (!user) {
